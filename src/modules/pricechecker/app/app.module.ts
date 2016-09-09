16964b7d97efa3639f3cb1fc7bee03eb0d39b80c
Nonico Bugash <nonico_bugash@rapid7.com>
2016-09-08 23:50:52 -0400

    Fix error

--- src/modules/pricechecker/app/app.module.ts
+++ src/modules/pricechecker/app/app.module.ts
@@ -5,8 +5,8 @@ import { NgModule, NgModuleMetadataType } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component'
 @NgModule(<NgModuleMetadataType>{
-    imports: [ BrowserModule ],
-    declaration: [ AppComponent ],
-    bootstrap: [ AppComponent ]
+    imports:      [ BrowserModule ],
+    declarations: [ AppComponent ],
+    bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
